6646ae2d59df57859946d45c2957576da7dbdb3a
Ahmed <darwish144.ad@gmail.com>
2021-06-08 10:00:45 +0300

    More js and challenges

--- css/myjs.js
+++ css/myjs.js
@@ -60,3 +60,25 @@ function loveCal(fname, sname) {
 
 
 console.log(loveCal("Ahmed", "Darwish"));
+
+// Leap year challenge!
+
+function leapYear(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0 ) {
+            if (year % 400 === 0) {
+                return "A leap Year!"
+            }
+
+        }else {
+            return "A leap Year! "
+        }
+
+    }else {
+        return "Not a leap Year"
+    }
+
+
+}
+
+leapYear(2001);
